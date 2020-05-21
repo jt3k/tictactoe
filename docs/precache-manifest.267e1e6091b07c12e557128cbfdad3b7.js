af3bb9a3657cbb75ca0dc8f9133c0374cf95dceb
Andrey Gurtovoy <iyntx@ya.ru>
2020-05-22 00:30:48 +0300

    Fixed demo

---
+++ docs/precache-manifest.267e1e6091b07c12e557128cbfdad3b7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf3fdf7af60a294d6d3f48cb7ad82488",
+    "url": ".assets/icon.png"
+  },
+  {
+    "revision": "79af02ac32f611a7b2c5",
+    "url": ".bundle.18393.css"
+  },
+  {
+    "revision": "79af02ac32f611a7b2c5",
+    "url": ".bundle.79af0.js"
+  },
+  {
+    "revision": "b7d607e9c3c7c18c5a59022793058a00",
+    "url": ".index.html"
+  },
+  {
+    "revision": "9566ad6c27c7395c5f46",
+    "url": ".polyfills.9566a.js"
+  },
+  {
+    "revision": "144c6ff1323d2d15182a7d2af08d027e",
+    "url": ".sw-debug.js"
+  },
+  {
+    "revision": "8255229e674a5ab0bbb9e890db043e0e",
+    "url": ".sw-esm.js"
+  },
+  {
+    "revision": "6303165b378d58fb2127c91a94eec2d5",
+    "url": ".sw.js"
+  }
+]);
---
+++ docs/precache-manifest.b9c8c47f4c315ee8c4d9c38c3620f64d.esm.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf3fdf7af60a294d6d3f48cb7ad82488",
+    "url": ".assets/icon.png"
+  },
+  {
+    "revision": "79af02ac32f611a7b2c5",
+    "url": ".bundle.18393.css"
+  },
+  {
+    "revision": "79af02ac32f611a7b2c5",
+    "url": ".bundle.e39e2.esm.js"
+  },
+  {
+    "revision": "b7d607e9c3c7c18c5a59022793058a00",
+    "url": ".index.html"
+  },
+  {
+    "revision": "9566ad6c27c7395c5f46",
+    "url": ".polyfills.c0b1d.esm.js"
+  }
+]);
