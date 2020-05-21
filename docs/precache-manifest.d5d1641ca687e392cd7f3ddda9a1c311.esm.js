63d63c8d5e80685c8cd5ec51d4a8f13e448015ba
Andrey Gurtovoy <iyntx@ya.ru>
2020-05-21 23:37:59 +0300

    Added github-pages site

---
+++ docs/precache-manifest.d5d1641ca687e392cd7f3ddda9a1c311.esm.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf3fdf7af60a294d6d3f48cb7ad82488",
+    "url": "/assets/icon.png"
+  },
+  {
+    "revision": "c0b86063eeeb99c8ad35",
+    "url": "/bundle.10928.css"
+  },
+  {
+    "revision": "c0b86063eeeb99c8ad35",
+    "url": "/bundle.1e27b.esm.js"
+  },
+  {
+    "revision": "605092146c3375d1daf7c10e6fe9b24a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b96b28270e3dc7238eca",
+    "url": "/polyfills.7484f.esm.js"
+  }
+]);
